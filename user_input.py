9e5d702821639351b296f59d9f82e64d0f830224
Sheyzie <oluwaseyiolukosi7@gmail.com>
2024-11-02 21:40:45 +0100

    Week 1 assignment completed

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Requesting user input for name
+name = input('Please enter your name: ')
+
+# Requesting user input for age and converting input to integer
+age = int(input('Please enter your age: '))
+
+# Requesting user input for location
+location = input('Please enter your location: ')
+
+# Printing out a personalized message using the user's name, age, and location.
+print(f'Hello {name}, you are {age} years old and presently located at {location}')
